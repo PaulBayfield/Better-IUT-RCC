71a947a20749b70bd6248713d41203131ed604ba
Simon Ledoux <simon@simon511000.fr>
2024-04-06 06:06:10 +0000

    feat: Ajout de la compatibilité avec Firefox

--- src/main.js
+++ src/main.js
@@ -6,52 +6,49 @@ import { addButtons, addSaveButton, ajouterBoutonSauvegarde, applyStyle, cleanCa
 import { Utils } from './utils';
 import * as browser from 'webextension-polyfill';
 
-// Lancement du script une fois la page chargée
-window.addEventListener('load', () => {
-    // Récupération des notes connues
-    browser.storage.sync.get('notesAlreadyKnow').then((result) => {
-        const notesConnues = result.notesAlreadyKnow || []
+// Récupération des notes connues
+browser.storage.sync.get('notesAlreadyKnow').then((result) => {
+    const notesConnues = result.notesAlreadyKnow || []
 
-        const tableau = document.querySelector("#mainContent > div.row > div:nth-child(4) > div > div > table")
-        let notes = recupererToutesLesNotesTriees(tableau)
+    const tableau = document.querySelector("#mainContent > div.row > div:nth-child(4) > div > div > table")
+    let notes = recupererToutesLesNotesTriees(tableau)
 
-        let tableauTrie = recreerTableau(notes, notesConnues)
+    let tableauTrie = recreerTableau(notes, notesConnues)
 
-        tableau.replaceWith(tableauTrie)
+    tableau.replaceWith(tableauTrie)
 
 
-        const boutonSauvegarde = ajouterBoutonSauvegarde()
-        // Quand le bouton est cliqué, on sauvegarder les notes, puis en recrée le tableau
-        boutonSauvegarde.addEventListener('click', (e) => {
-            e.preventDefault();
+    const boutonSauvegarde = ajouterBoutonSauvegarde()
+    // Quand le bouton est cliqué, on sauvegarder les notes, puis en recrée le tableau
+    boutonSauvegarde.addEventListener('click', (e) => {
+        e.preventDefault();
 
-            let ids = []
-            notes.forEach((note) => {
-                ids.push(note.id)
-            })
+        let ids = []
+        notes.forEach((note) => {
+            ids.push(note.id)
+        })
 
-            browser.storage.sync.set({notesAlreadyKnow: ids}).then(() => {
-                location.reload()
-            })
+        browser.storage.sync.set({notesAlreadyKnow: ids}).then(() => {
+            location.reload()
         })
     })
+})
 
-    // Récupération des données et génération du graphique
-    const averageDataByUE = getAverage();
-    const title = document.querySelector("#mainContent > div > div:nth-child(6) > div > h4");
+// Récupération des données et génération du graphique
+const averageDataByUE = getAverage();
+const title = document.querySelector("#mainContent > div > div:nth-child(6) > div > h4");
 
-    if (!Utils.isEmpty(title, averageDataByUE) && title.textContent === "Modalités de Contrôle des Connaissances") {
-        generateHtml(averageDataByUE);
-    }
+if (!Utils.isEmpty(title, averageDataByUE) && title.textContent === "Modalités de Contrôle des Connaissances") {
+    generateHtml(averageDataByUE);
+}
 
-    // Nettoyage des cartes et réorganisation
-    cleanCards();
-    orderCards();
+// Nettoyage des cartes et réorganisation
+cleanCards();
+orderCards();
 
-    // Création de la carte de bilan
-    createBilanCard();
+// Création de la carte de bilan
+createBilanCard();
 
-    // Ajout des styles et des boutons
-    applyStyle();
-    addButtons();
-});
+// Ajout des styles et des boutons
+applyStyle();
+addButtons();
