fe68c7cd6fa1357efdd6b1ea95281d963bef58b6
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-11-12 09:56:19 +0100

    fix: Correction de l'ouverture de la page des paramètres

--- src/background.js
+++ src/background.js
@@ -1,23 +1,13 @@
 import * as browser from 'webextension-polyfill';
 
 
-console.log("Better IUT RCC background task started!");
+console.log("[Better IUT RCC] Background script lancé");
 
 
 // Ajout d'un listener pour ouvrir la popup/paramètres
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.openOptions) {
-        const url = browser.runtime.getURL("popup.html");
-        if (url.includes("chrome-extension://")) {
-            try {
-                console.log("[Better IUT RCC] Opening options page...");
-                chrome.action.openPopup(); // Google Chrome 127+
-            } catch (e) {
-                console.log("[Better IUT RCC] Opening options page... (fallback)");
-                chrome.browserAction.openPopup(); // Google Chrome 126-
-            }
-        } else {
-            alert("L'ouverture de la page des paramètres n'est pas supportée sur Firefox pour le moment. Veuillez ouvrir la page des paramètres manuellement.");
-        }
+        console.info("[Better IUT RCC] Ouverture des paramètres");
+        browser.runtime.openOptionsPage()
     }
 })
